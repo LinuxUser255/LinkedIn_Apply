230b4689fbcd80754327cbff6f09fe79d9a35557
Chris <atnl@protonmail.com>
2024-10-28 18:05:47 -0400

    Update easyapply_mod.py

--- easyapply_mod.py
+++ easyapply_mod.py
@@ -1,9 +1,7 @@
 """
-The job of this module is to use the xpath value to find 
+The job of this module is to use the xpath value to find
 and click the easy apply button on a LinkedIn job listing page.
 """
-from random import randint
-
 
 from selenium.webdriver.common.by import By
 
@@ -18,13 +16,12 @@ class ApplyButtonClicker:
         print('Using regular XPath...')
         try:
             # accept any number for the ember element ID
-            easy_apply_button = self.driver.find_element(By.XPATH, f'//button[@id="ember{randint(1, 1000)}"]')
+            easy_apply_button = self.driver.find_element(By.XPATH, '//button[starts-with(@id, "ember")]')
             if easy_apply_button:
                 easy_apply_button.click()
                 print('Easy apply button clicked using regular XPath.')
             else:
                 print('Easy apply button not found using regular XPath.')
-            # easy_apply_button = self.driver.find_element(By.XPATH, '//*[@id="ember148"]/svg')
             if easy_apply_button:
                 easy_apply_button.click()
                 print('Easy apply button clicked using regular XPath.')
@@ -33,45 +30,27 @@ class ApplyButtonClicker:
         except Exception as e:
             print(f'Error clicking easy apply button using regular XPath: {e}')
 
-    # method to find and click the easy apply button using the Full Xpath
-    def click_easy_apply_button_full_xpath(self):
-        print('Using full XPath...')
-        try:
-            easy_apply_button = self.driver.find_element(By.XPATH, '/html/body/div[6]/div[3]/div['
-                                                                   '4]/div/div/main/div/div[2]/div[2]/div/div['
-                                                                   '2]/div/div[1]/div/div[1]/div/div[1]/div/div['
-                                                                   '6]/div/div/div/button/svg')
-            if easy_apply_button:
-                # easy_apply_button
-                easy_apply_button.click()
-                print('Easy apply button clicked using full XPath.')
-        except Exception as e:
-            print(f'Error clicking easy apply button using full XPath: {e}')
-
-    # Method uses the page element find for the easy apply button using CSS selector
+    # Method uses the page element find for the easy apply button using The Element selector
     def click_easy_apply_button_element(self):
         print('Trying Element...')
         try:
-            easy_apply_button = self.driver.find_element(By.CSS_SELECTOR, 'svg[role="none"][aria-hidden="true"]['
-                                                                          'class="artdeco-button__icon "]')
+            # the element to search for and click:
+            # <button aria-label="Easy Apply to " id="ember" class="jobs-apply-button artdeco-button artdeco-button--3 artdeco-button--primary ember-view">
+            # <svg role="none" aria-hidden="true" class="artdeco-button__icon artdeco-button__icon--in-bug" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" data-supported-dps="14x14" data-test-icon="linkedin-bug-xxsmall">
+            # <!---->
+            #     <use href="#linkedin-bug-xxsmall" width="14" height="14"></use>
+            # </svg>
+            #
+            #
+            # <span class="artdeco-button__text">
+            #     Easy Apply
+            # </span></button>
+            easy_apply_button = self.driver.find_element(By.CSS_SELECTOR, '#ember > svg[role="none"][aria-hidden="true"]''.artdeco-button__icon')
             if easy_apply_button:
                 easy_apply_button.click()
-                print('Easy apply button clicked using CSS selector.')
+                print('Easy apply button clicked using Element Selector.')
             else:
-                print('Easy apply button not found using CSS selector.')
+                print('Easy apply button Not found using Element Selector.')
         except Exception as e:
-            print(f'Error clicking easy apply button using CSS selector: {e}')
+            print(f'Error clicking easy apply button using Element selector: {e}')
 
-    def click_easy_apply_button_js_path(self):
-        print('Trying JavaScript...')
-        # document.querySelector("#ember1084 > svg")
-        try:
-            easy_apply_button = self.driver.execute_script(
-                "return document.querySelector('#ember1084 > svg')")
-            if easy_apply_button:
-                easy_apply_button.click()
-                print('Easy apply button clicked using JavaScript path.')
-            else:
-                print('JavaScript path: Easy apply Button Not Found!')
-        except Exception as e:
-            print(f'Error using JavaScript path: {e}')
